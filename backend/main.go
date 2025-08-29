02b9eeb816c561da2302dd99ae8750620a2f4e69
Fırat Tuna Arslan <firattunaarslan@gmail.com>
2025-08-29 19:03:05 +0300

    main.go

---
+++ backend/main.go
@@ -0,0 +1,19 @@
+main.go
+
+package main
+
+import (
+    "log"
+    "net/http"
+)
+
+func main() {
+    InitDB()
+
+    http.Handle("/", http.FileServer(http.Dir("../frontend")))
+    http.HandleFunc("/api/register", RegisterHandler)
+    http.HandleFunc("/api/login", LoginHandler)
+
+    log.Println("Server running on http://localhost:8080")
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
